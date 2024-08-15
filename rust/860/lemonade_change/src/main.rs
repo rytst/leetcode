f9e3709cf27b42a14d291643a2c8b9f27b8267a7
rytst <r.saito0111@gmail.com>
2024-08-16 03:49:52 +0900

    sleepy

---
+++ rust/860/lemonade_change/src/main.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+
+impl Solution {
+    pub fn lemonade_change(bills: Vec<i32>) -> bool {
+        let mut chages = HashMap::new();
+
+
+        for i in 0..bills.len() {
+            if bills[i] == 5 {
+                changes.insert(5, )
+            } else if bills[i] == 10 {
+
+            } else {
+
+            }
+        }
+    }
+}
+
+
