c7de175080e6de0a79838d33fd9d81e41b8524da
rytst <r.saito0111@gmail.com>
2024-08-19 17:49:16 +0900

    lemonade

--- rust/860/lemonade_change/src/main.rs
+++ rust/860/lemonade_change/src/main.rs
@@ -1,21 +1,58 @@
-use std::collections::HashMap;
+struct LemonadeStand {
+    fives: i32,
+    tens: i32,
+}
 
+impl LemonadeStand {
+    fn new() -> Self {
+        LemonadeStand { fives: 0, tens: 0 }
+    }
 
-impl Solution {
-    pub fn lemonade_change(bills: Vec<i32>) -> bool {
-        let mut chages = HashMap::new();
-
-
-        for i in 0..bills.len() {
-            if bills[i] == 5 {
-                changes.insert(5, )
-            } else if bills[i] == 10 {
-
-            } else {
-
+    fn can_provide_change(&mut self, bills: Vec<i32>) -> bool {
+        for bill in bills {
+            if !self.process_transaction(bill) {
+                return false;
             }
         }
+        true
+    }
+
+    fn process_transaction(&mut self, bill: i32) -> bool {
+        match bill {
+            5 => {
+                self.fives += 1;
+                true
+            }
+            10 => {
+                if self.fives == 0 {
+                    return false;
+                }
+                self.fives -= 1;
+                self.tens += 1;
+                true
+            }
+            20 => self.provide_change_for_20(),
+            _ => false, 
+        }
+    }
+
+    fn provide_change_for_20(&mut self) -> bool {
+        if self.tens > 0 && self.fives > 0 {
+            self.tens -= 1;
+            self.fives -= 1;
+        } else if self.fives >= 3 {
+            self.fives -= 3;
+        } else {
+            return false;
+        }
+        true
     }
 }
 
 
+
+impl Solution {
+    pub fn lemonade_change(bills: Vec<i32>) -> bool {
+        LemonadeStand::new().can_provide_change(bills)
+    }
+}
